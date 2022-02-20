82965964c92672fff44da96d5ac94e07f078a7da
Dvlv <Dvlv@users.noreply.github.com>
2022-02-20 14:03:31 +0000

    ms version

--- web/views/blueprints.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-game_blueprint = Blueprint("game", __name__, url_prefix="/game")
-
-record_blueprint = Blueprint("record", __name__, url_prefix="/record")
-
-runner_blueprint = Blueprint("runner", __name__, url_prefix="/runner")
--- web/views/runner/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-
-class RunnerForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    country = StringField("Country")
-
