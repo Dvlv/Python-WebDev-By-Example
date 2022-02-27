47363390d1570cff9dafb5543d57f47e92a09807
Dvlv <Dvlv@users.noreply.github.com>
2022-02-27 12:01:54 +0000

    finish ch1

---
+++ code/models/__init__.py
@@ -0,0 +1,5 @@
+from peewee import Model
+
+
+class BaseModel(Model):
+    pass
--- code/web_server.py
+++ code/web_server.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 app.secret_key = "very secret"
 
 
 @app.route("/")
-def hello_flask():
+def index():
     products = {"toothpaste": 2.00, "toothbrush": 1.50, "floss": 0.99}
     return render_template("index.html", products=products)
+
+
+@app.route("/<string:product_name>")
+def view_product(product_name):
+    products = {"toothpaste": 2.00, "toothbrush": 1.50, "floss": 0.99}
+
+    price = None
+    if product_name in products:
+        price = products[product_name]
+    else:
+        abort(404)
+
+    return render_template(
+        "products/view_product.html", price=price, product_name=product_name
+    )
 
 
 if __name__ == "__main__":
