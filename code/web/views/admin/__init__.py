31b6604858ec07f6bc3a71c1806ae74f99ba74b3
Dvlv <Dvlv@users.noreply.github.com>
2022-03-20 10:45:17 +0000

    WIP begin ch4 writing

--- code/web/views/admin/__init__.py
+++ code/web/views/admin/__init__.py
@@ -1,5 +1,12 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request, session, redirect, url_for
 
 from web.blueprints import admin_blueprint
 
 from . import admin
+
+
+@admin_blueprint.before_request
+def admin_before_request():
+    if request.endpoint != "admin.admin_login":
+        if "logged_in" not in session:
+            return redirect(url_for("admin.admin_login"))
--- code/web_server.py
+++ code/web_server.py
@@ -3,10 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = "very secret"
 
-
-from web.views.admin import admin
-from web.views.site import site
-
+from web.views import admin, site
 from web.blueprints import site_blueprint, admin_blueprint
 
 app.register_blueprint(site_blueprint)
